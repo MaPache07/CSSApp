4ffbe3e7cd48f25774bcf01f7a8f5116f2f97b68
The5econd <00017917@uca.edu.sv>
2020-12-21 14:20:36 -0600

    trabajando en pestaña configuraciones, correo de sesion actual agregado. retoques extra en algunos .xml

--- app/src/main/java/com/uca/app_css/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/uca/app_css/ui/settings/SettingsFragment.kt
@@ -1,12 +1,17 @@
 package com.uca.app_css.ui.settings
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.AlarmClock
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.getBitmap
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -20,6 +25,8 @@ class SettingsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var pfp: ImageView
+    private lateinit var mail: TextView
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -28,9 +35,10 @@ class SettingsFragment : Fragment() {
         notificationsViewModel =
                 ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-
         val logOutBtn:Button = root.findViewById(R.id.logout_btn)
         mAuth = FirebaseAuth.getInstance()
+        pfp = root.findViewById(R.id.profile_pic)
+        mail = root.findViewById(R.id.info_email)
 
         logOutBtn.setOnClickListener {
             mAuth.signOut()
@@ -40,6 +48,11 @@ class SettingsFragment : Fragment() {
             }
             startActivity(intent)
         }
+
+        mail.text = mAuth.currentUser!!.email
+        Log.d("testing", "callate kk")
+        
+
         return root
     }
 }
