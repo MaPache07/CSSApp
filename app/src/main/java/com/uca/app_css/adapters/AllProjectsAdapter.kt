8f12c03f70ddbe09ad508b26afa7feba6175ac02
The5econd <00017917@uca.edu.sv>
2020-12-28 11:59:09 -0600

    Adapater del recycler terminado

--- app/src/main/java/com/uca/app_css/adapters/AllProjectsAdapter.kt
+++ app/src/main/java/com/uca/app_css/adapters/AllProjectsAdapter.kt
@@ -7,45 +7,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.uca.app_css.R
 //import com.bumptech.glide.Glide
 import com.uca.app_css.database.entities.Proyecto
 
-class AllProjectsAdapter(val context: Context, val projects: List<Proyecto>): RecyclerView.Adapter<AllProjectsAdapter.ProyectoHolder>() {
+class AllProjectsAdapter(val projects: List<Proyecto>, val clickListener: (Proyecto) -> Unit): RecyclerView.Adapter<AllProjectsAdapter.ProyectoHolder>() {
 
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): AllProjectsAdapter.ProyectoHolder  {
-        val view = LayoutInflater.from(context).inflate(R.layout.cardview_all_projects, p0, false)
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ProyectoHolder  {
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.cardview_all_projects, p0, false)
         return ProyectoHolder(view)
     }
 
     override fun onBindViewHolder(proyectoHolder: ProyectoHolder, pos: Int) {
         //aca para pasarle todo lo que se quiere
-        //proyectoHolder.proyectoName.text =
-        //en caso de poner imagen usar glide
-        //Glide.with(context).load(projects.get(pos).image).into(proyectoHolder.projectsImage)
+        proyectoHolder.bind(projects[pos],clickListener)
     }
 
-    override fun getItemCount(): Int {
-        return projects.size
-    }
+    override fun getItemCount(): Int = projects.size
 
-    class ProyectoHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
-        //funcion para que al darle click oase a la otra actividad
-        //donde se detalle la info de cada proyecto
 
-        init{
-            v.setOnClickListener(this)
+    inner class ProyectoHolder constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item : Proyecto, clickListener: (Proyecto) -> Unit) = with(itemView){
+            itemView.findViewById<TextView>(R.id.nombre_project_individual).text = item.nombre
+            this.setOnClickListener{clickListener(item)}
         }
 
-        override fun onClick(v: View) {
-           /* var container: LinearLayout = v as LinearLayout
-            var mIntent = Intent(v.context, ProyectoActivity::class.java)
-            mIntent.putExtra("URL",(container.getChildAt(1) as TextView).hint.toString())
-            v.context.startActivity(mIntent)
-            */
-        }
 
         //val proyecto = v.nombreTextView
         //para pasar la imagen con el intent
