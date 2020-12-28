1f0aa3df23adc9edcd7a5b7f3f50bca0a9fc0577
MaPache07 <00170517@uca.edu.sv>
2020-12-28 13:03:38 -0600

    Se ha implementado parcelable en la entidad proyecto

--- app/src/main/java/com/uca/app_css/database/entities/Proyecto.kt
+++ app/src/main/java/com/uca/app_css/database/entities/Proyecto.kt
@@ -1,24 +1,76 @@
 package com.uca.app_css.database.entities
 
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 @Entity(tableName = "proyecto")
 data class Proyecto(
     @PrimaryKey
     val idProyecto: Int,
     val estado: Boolean,
-    val contraparte: String,
+    val contraparte: String?,
     val cupos: Int,
-    val descripcion: String,
-    val encargado: String,
-    val fecha_inicio: String,
-    val fecha_fin: String,
-    val horario: String,
-    val nombre: String,
-    val tipo_horas: String,
-    val modifiedAt: String,
-    val modifiedBy: String,
-    val createdAt: String
-)
+    val descripcion: String?,
+    val encargado: String?,
+    val fecha_inicio: String?,
+    val fecha_fin: String?,
+    val horario: String?,
+    val nombre: String?,
+    val tipo_horas: String?,
+    val modifiedAt: String?,
+    val modifiedBy: String?,
+    val createdAt: String?
+) : Parcelable{
+    @RequiresApi(Build.VERSION_CODES.Q)
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readBoolean(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(idProyecto)
+        dest.writeBoolean(estado)
+        dest.writeString(contraparte)
+        dest.writeInt(cupos)
+        dest.writeString(descripcion)
+        dest.writeString(encargado)
+        dest.writeString(fecha_inicio)
+        dest.writeString(fecha_fin)
+        dest.writeString(horario)
+        dest.writeString(nombre)
+        dest.writeString(tipo_horas)
+        dest.writeString(modifiedAt)
+        dest.writeString(modifiedBy)
+        dest.writeString(createdAt)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Proyecto>{
+        @RequiresApi(Build.VERSION_CODES.Q)
+        override fun createFromParcel(parcel: Parcel): Proyecto {
+            return Proyecto(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Proyecto?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
