9a2b9d3e84c2036f139977b0b2595251a93cfeb0
The5econd <00017917@uca.edu.sv>
2020-12-28 13:04:50 -0600

    actividad de informacion de cada proyecto agregada

--- app/src/main/java/com/uca/app_css/ProjectInfoActivity.kt
+++ app/src/main/java/com/uca/app_css/ProjectInfoActivity.kt
@@ -2,10 +2,15 @@ package com.uca.app_css
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.uca.app_css.database.entities.Proyecto
 
 class ProjectInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_info)
+        //val proyecto : Proyecto = intent.extras.getParcelable<>()
+        var test2: TextView = findViewById(R.id.test2)
+        
     }
 }
